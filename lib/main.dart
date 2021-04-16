f20c0a517a075f53a65c10d5c760b22c0004f77d
Aarush-Goyal <goyalaarush92@gmail.com>
2021-04-16 22:38:43 +0530

    🏗️ updated ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,37 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.black12,
+        appBar: AppBar(
+          title: Text('Konichiva'),
+          backgroundColor: Colors.black12,
+          shadowColor: Colors.transparent,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.blue,
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
